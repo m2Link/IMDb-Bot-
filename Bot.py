e3a59cf62be83846939be3522e1eba2a831d9877
m2Link <88938300+m2Link@users.noreply.github.com>
2021-11-02 20:54:15 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -11,7 +11,6 @@ from info import API_HASH
 from info import BOT_TOKEN
 from OMDB import get_movie_info
 #====================================================================
-        startbuttons=[[InlineKeyboardButton('🔍 𝖲𝖾𝖺𝗋𝖼𝗁 𝖮𝗇 𝖦𝗈𝗈𝗀𝗅𝖾.', url=f'https://google.com/search?q={movie_name.replace(" ","+")}')]]
         await message.reply_text(text="𝖢𝗈𝗎𝗅𝖽𝗇'𝗍 𝖥𝖾𝗍𝖼𝗁 𝖣𝖾𝗍𝖺𝗂𝗅𝗌\n𝖳𝗋𝗒 𝖳𝗈 𝖢𝗁𝖾𝖼𝗄 𝖸𝗈𝗎𝗋 𝖲𝗉𝖾𝗅𝗅𝗂𝗇𝗀.", reply_markup=InlineKeyboardMarkup(startbuttons))
 
 #=======================================================================
